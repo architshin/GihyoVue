caf65f45fc1e79b61ec2cd7191c946ccaffc2e68
Shinzo SAITO <architshin@websarva.com>
2021-10-07 19:29:01 +0900

    directive-bind-booleanを追加。

---
+++ chap04/directive-bind-boolean/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<p><button type="button" v-bind:disabled="isSendButtonDisabled">送信</button></p>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const isSendButtonDisabled = ref(true);
+		return {
+			isSendButtonDisabled
+		};
+	}
+});
+</script>
