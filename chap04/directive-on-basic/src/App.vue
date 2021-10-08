6c5b85056e54e95c6087b507bb3039e6fe104f30
Shinzo SAITO <architshin@websarva.com>
2021-10-09 04:28:09 +0900

    directive-on-basicの追加。

---
+++ chap04/directive-on-basic/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<section>
+		<button v-on:click="onButtonClick">クリック!</button>
+		<p>クリックの結果: {{randValue}}</p>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const randValue = ref("まだです");
+		const onButtonClick = function(): void {
+			const rand = Math.round(Math.random() * 10);
+			randValue.value = String(rand);
+		}
+		return {
+			randValue,
+			onButtonClick,
+		};
+	}
+});
+</script>
