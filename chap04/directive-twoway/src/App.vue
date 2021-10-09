6bf1822e626bbc9d957ecc0db7fab91ab25d5ad2
Shinzo SAITO <architshin@websarva.com>
2021-10-10 04:17:44 +0900

    directive-twowayの追加。

---
+++ chap04/directive-twoway/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<section>
+		<input type="text" v-bind:value="inputName2Way" v-on:input="onInputName2Way">
+		<p>{{inputName2Way}}</p>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const inputName2Way = ref("双方向");
+		const onInputName2Way = function(event: Event): void {
+			const element = event.target as HTMLInputElement;
+			inputName2Way.value = element.value;
+		}
+		return {
+			inputName2Way,
+			onInputName2Way
+		};
+	}
+});
+</script>
