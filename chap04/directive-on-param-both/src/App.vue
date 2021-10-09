51a20ecbf41a3666b29ccd1e2a90ec77697aafa2
Shinzo SAITO <architshin@websarva.com>
2021-10-10 02:14:00 +0900

    directive-on-param-bothを追加。

---
+++ chap04/directive-on-param-both/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<p v-on:click="onPClickWithEvent('green', $event)" v-bind:style="{backgroundColor: pBgColorEvent}">
+		{{pMsg}}
+	</p>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const pMsg = ref("イベント前(ここをクリック!)");
+		const pBgColorEvent = ref("white");
+		const onPClickWithEvent = function(bgColor: string, event: MouseEvent): void {
+			pBgColorEvent.value = bgColor;
+			pMsg.value = event.timeStamp.toString();
+		};
+		return {
+			pMsg,
+			pBgColorEvent,
+			onPClickWithEvent
+		};
+	}
+});
+</script>
