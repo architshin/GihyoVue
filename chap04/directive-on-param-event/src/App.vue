49481455063f56514091f13d229b95f04848bde4
Shinzo SAITO <architshin@websarva.com>
2021-10-09 16:47:43 +0900

    directive-on-param-eventを追加。

---
+++ chap04/directive-on-param-event/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<section>
+		<img src="./assets/logo.png" alt="Vueのロゴ" v-on:mousemove="onImgMousemove">
+		<p>ポインタの位置: x={{mousePointerX}}; y={{mousePointerY}}</p>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const mousePointerX = ref(0);
+		const mousePointerY = ref(0);
+		const onImgMousemove = function(event: MouseEvent): void {
+			mousePointerX.value = event.offsetX;
+			mousePointerY.value = event.offsetY;
+		}
+		return {
+			mousePointerX,
+			mousePointerY,
+			onImgMousemove
+		};
+	}
+});
+</script>
