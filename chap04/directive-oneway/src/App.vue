db769a5b1059aa97adeba487f8759c6b9301bd3c
Shinzo SAITO <architshin@websarva.com>
2021-10-10 04:04:31 +0900

    directive-onewayの追加。

---
+++ chap04/directive-oneway/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<section>
+		<input type="text" v-bind:value="inputNameBind">
+	</section>
+	<br>
+	<section>
+		<input type="text" v-on:input="onInputName">
+		<p>{{inputNameOn}}</p>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const inputNameBind = ref("しんちゃん");
+		const inputNameOn = ref("ななし");
+		const onInputName = function(event: Event): void {
+			const element = event.target as HTMLInputElement;
+			inputNameOn.value = element.value;
+		}
+		return {
+			inputNameBind,
+			inputNameOn,
+			onInputName
+		};
+	}
+});
+</script>
