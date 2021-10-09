f5f4e0cb365e4595d52d13fd6a618e391f49dcb6
Shinzo SAITO <architshin@websarva.com>
2021-10-10 03:36:11 +0900

    directive-on-modifiersの追加。

---
+++ chap04/directive-on-modifiers/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<p>{{msg}}</p>
+	<input type="text" v-on:keydown.enter="onEnterKey"><br>
+	<button v-on:click.right="onRightButtonClick">右クリック</button><br>
+	<button v-on:click.shift="onShiftClick">シフトを押しながらクリック</button><br>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const msg = ref("まだです!");
+		const onEnterKey = function(): void {
+			msg.value = "エンターキーが押下されました。";
+		};
+		const onRightButtonClick = function(): void {
+			msg.value = "ボタンが右クリックされました。";
+		};
+		const onShiftClick = function(): void {
+			msg.value = "シフトを押しながらクリックされました。";
+		};
+		return {
+			msg,
+			onEnterKey,
+			onRightButtonClick,
+			onShiftClick
+		};
+	}
+});
+</script>
