b4dc7ed2db6f6b5f8a701a9a900925114ed5cf9d
Shinzo SAITO <architshin@websarva.com>
2021-10-10 19:00:49 +0900

    directive-htmlの追加。

---
+++ chap04/directive-html/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<section>{{htmlStr}}</section>
+	<section v-html="htmlStr"></section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const htmlStr = ref(`<a href="https://v3.vuejs.org/">Vue.jsのTOPページ<a/>`);
+		return {
+			htmlStr
+		};
+	}
+});
+</script>
