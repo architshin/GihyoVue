a14b6bd06fccf4a4d41de0c13a34cf4ae5654bb8
Shinzo SAITO <architshin@websarva.com>
2021-10-10 19:13:39 +0900

    directive-preの追加。

---
+++ chap04/directive-pre/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<section v-pre>
+		<p v-on:click="showHello">{{hello!}}</p>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+});
+</script>
