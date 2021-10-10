8d857dc263132601be14aa0c7f1743e69b6a0427
Shinzo SAITO <architshin@websarva.com>
2021-10-10 19:35:14 +0900

    directive-cloakの追加。

---
+++ chap04/directive-cloak/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<p v-cloak>{{hello}}</p>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const hello = ref("こんにちは!");
+		return {
+			hello
+		};
+	}
+});
+</script>
+
+<style>
+[v-cloak] {
+	display: none;
+}
+</style>
