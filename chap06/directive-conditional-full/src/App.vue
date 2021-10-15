434be153d14f7c5041d0ac5706288d40db7e9b2d
Shinzo SAITO <architshin@websarva.com>
2021-10-16 03:28:36 +0900

    directive-conditional-fullの追加。

---
+++ chap06/directive-conditional-full/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<p>
+		点数は{{randomNumber}}点で
+		<span v-if="randomNumber >= 80">優です。</span>
+		<span v-else-if="randomNumber >= 70">良です。</span>
+		<span v-else-if="randomNumber >= 60">可です。</span>
+		<span v-else>不可です。</span>
+	</p>
+</template>
+
+<script lang="ts">
+import {computed, defineComponent} from "vue";
+
+export default defineComponent({
+	setup() {
+		const randomNumber = computed(
+			function(): number {
+				return Math.round(Math.random() * 100);
+			}
+		);
+		return {
+			randomNumber
+		};
+	}
+});
+</script>
