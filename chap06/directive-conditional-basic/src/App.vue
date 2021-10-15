9ab8571295ad9c6f531434e7a84ccd2b69285a8a
Shinzo SAITO <architshin@websarva.com>
2021-10-16 02:38:24 +0900

    コメントに。を追加。

--- chap06/directive-conditional-basic/src/App.vue
+++ chap06/directive-conditional-basic/src/App.vue
@@ -18,11 +18,11 @@ export default defineComponent({
 		const number = ref(80);
 		const showOrNot = computed(
 			function(): boolean {
-				//戻り値用変数を初期値falseで用意
+				//戻り値用変数を初期値falseで用意。
 				let showOrNot = false;
-				//0〜100の乱数を発生
+				//0〜100の乱数を発生。
 				const rand = Math.round(Math.random() * 100);
-				//乱数が50以上ならば、戻り値をtrueに変更
+				//乱数が50以上ならば、戻り値をtrueに変更。
 				if(rand >= 50) {
 					showOrNot = true;
 				}
