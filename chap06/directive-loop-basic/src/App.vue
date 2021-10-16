8638be7f3df717f4a3417e2ed80695ace5d59170
Shinzo SAITO <architshin@websarva.com>
2021-10-17 03:57:35 +0900

    directive-loop-basicの追加。

---
+++ chap06/directive-loop-basic/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<ul>
+		<li
+			v-for="cocktailName in cocktailList"
+			v-bind:key="cocktailName">
+			{{cocktailName}}
+		</li>
+	</ul>
+	<ul>
+		<li
+			v-for="(cocktailName, index) in cocktailList"
+			v-bind:key="cocktailName">
+			{{cocktailName}}(インデックス{{index}})
+		</li>
+	</ul>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const cocktailList = ref(["ホワイトレディ", "ブルーハワイ", "ニューヨーク"]);
+		return {
+			cocktailList
+		};
+	}
+});
+</script>
