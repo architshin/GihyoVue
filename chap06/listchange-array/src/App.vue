8c550fc262e1ddfdbeda1ceae28009b2be24b504
Shinzo SAITO <architshin@websarva.com>
2021-10-18 00:13:47 +0900

    listchange-arrayのインデントを修正。

--- chap06/listchange-array/src/App.vue
+++ chap06/listchange-array/src/App.vue
@@ -1,23 +1,23 @@
 <template>
-		<ul>
-			<li
-				v-for="(cocktailName, index) in cocktailList"
-				v-bind:key="cocktailName">
-				{{cocktailName}}(インデックス{{index}})
-			</li>
-		</ul>
-		<p>
-			CocktailListを
-			<button v-on:click="changeCocktailList">変更</button>
-		</p>
-		<p>
-			CocktailListの末尾に「ブルームーン」を
-			<button v-on:click="addCocktailList">追加</button>
-		</p>
-		<p>
-			CocktailListから末尾の要素を
-			<button v-on:click="deleteFromCocktailList">削除</button>
-		</p>
+	<ul>
+		<li
+			v-for="(cocktailName, index) in cocktailList"
+			v-bind:key="cocktailName">
+			{{cocktailName}}(インデックス{{index}})
+		</li>
+	</ul>
+	<p>
+		CocktailListを
+		<button v-on:click="changeCocktailList">変更</button>
+	</p>
+	<p>
+		CocktailListの末尾に「ブルームーン」を
+		<button v-on:click="addCocktailList">追加</button>
+	</p>
+	<p>
+		CocktailListから末尾の要素を
+		<button v-on:click="deleteFromCocktailList">削除</button>
+	</p>
 </template>
 
 <script lang="ts">
