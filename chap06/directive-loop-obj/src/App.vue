6c39f1c5034504e1a93d5099fd9fe7f6af4743a7
Shinzo SAITO <architshin@websarva.com>
2021-10-17 17:35:05 +0900

    directive-loop-objの追加。

---
+++ chap06/directive-loop-obj/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<dl>
+		<template
+			v-for="(value, key) in whiteLady"
+			v-bind:key="key">
+			<dt>{{key}}</dt>
+			<dd>{{value}}</dd>
+		</template>
+	</dl>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	setup() {
+		const whiteLadyInit: {
+			id: number;
+			name: string;
+			price: number;
+			recipe: string;
+		} = {
+			id: 2345,
+			name: "ホワイトレディ",
+			price: 1200,
+			recipe: "ジン30ml+コワントロー15ml+レモン果汁+15ml"
+		};
+		const whiteLady = ref(whiteLadyInit);
+		return {
+			whiteLady
+		};
+	}
+});
+</script>
