d1f31078d81e03913f4486907efc5039ddc3d10a
Shinzo SAITO <architshin@websarva.com>
2021-11-11 03:51:04 +0900

    components-basicsを追加。

---
+++ chap08/components-basics/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<h1>コンポーネント基礎</h1>
+	<section>
+		<h2>コンポーネント1個</h2>
+		<OneSection />
+	</section>
+	<section>
+		<h2>コンポーネントが複数</h2>
+		<OneSection />
+		<OneSection />
+		<OneSection />
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		OneSection
+	}
+});
+</script>
+
+<style>
+section {
+	border: blue 1px solid;
+	margin: 10px;
+}
+</style>
