6ed0a21afa58959800ed0560812ddc563f3207fb
Shinzo SAITO <architshin@websarva.com>
2021-11-11 17:59:13 +0900

    components-vmodelの追加。

---
+++ chap08/components-vmodel/src/components/WithModel.vue
@@ -0,0 +1,27 @@
+<template>
+	<section>
+		<p>{{name}}さんですね!</p>
+		<input type="text" v-model="name">
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	name: "WithModel",
+	setup() {
+		const name = ref("名無し");
+		return {
+			name
+		};
+	}
+});
+</script>
+
+<style scoped>
+section {
+	border: orange 1px solid;
+	margin: 10px;
+}
+</style>
