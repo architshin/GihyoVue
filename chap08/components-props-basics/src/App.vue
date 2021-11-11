9dcb72e935c5a65151efea74c2ed179b7e841b9d
Shinzo SAITO <architshin@websarva.com>
2021-11-12 00:49:00 +0900

    components-props-basicsの追加。

---
+++ chap08/components-props-basics/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<h1>Props基礎</h1>
+	<section>
+		<h2>属性に直接記述</h2>
+		<OneInfo
+			title="propsの利用"
+			content="子コンポーネントにデータを渡すにはpropsを利用する。"/>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import OneInfo from "./components/OneInfo.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		OneInfo
+	},
+});
+</script>
+
+<style>
+section {
+	border: blue 1px solid;
+	margin: 10px;
+}
+</style>
