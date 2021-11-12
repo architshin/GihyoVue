f314ad243c174d2c35224088b1def051c7c7e0c9
Shinzo SAITO <architshin@websarva.com>
2021-11-13 03:10:35 +0900

    components-props-bindの追加。

---
+++ chap08/components-props-bind/src/components/OneInfo.vue
@@ -0,0 +1,25 @@
+<template>
+	<section class="box">
+		<h4>{{title}}</h4>
+		<p>{{content}}</p>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+	name: "OneInfo",
+	props: {
+		title: String,
+		content: String
+	}
+});
+</script>
+
+<style scoped>
+.box {
+	border: green 1px solid;
+	margin: 10px;
+}
+</style>
