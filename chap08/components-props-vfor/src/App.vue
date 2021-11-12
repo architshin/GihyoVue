dfbb93ceefdf64373cf2514c82a25cb2ec12de9c
Shinzo SAITO <architshin@websarva.com>
2021-11-13 03:24:23 +0900

    components-props-vforの追加。

---
+++ chap08/components-props-vfor/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<h1>Props基礎</h1>
+	<section>
+		<h2>ループでコンポーネントを生成</h2>
+		<OneInfo
+			v-for="[id, weather] in weatherList"
+			v-bind:key="id"
+			v-bind:title="weather.title"
+			v-bind:content="weather.content"/>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import OneInfo from "./components/OneInfo.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		OneInfo
+	},
+	setup() {
+		const weatherListInit = new Map<number, Weather>();
+		weatherListInit.set(1, {id: 1, title: "今日の天気", content: "今日は一日中、晴でしょう。"});
+		weatherListInit.set(2, {id: 2, title: "明日の天気", content: "明日は一日中、雨でしょう。"});
+		weatherListInit.set(3, {id: 3, title: "明後日の天気", content: "明後日は一日中、雪でしょう。"});
+		const weatherList = ref(weatherListInit);
+		return {
+			weatherList
+		}
+	}
+});
+
+interface Weather {
+	id: number;
+	title: string;
+	content: string;
+}
+</script>
