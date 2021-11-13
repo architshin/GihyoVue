bff7a413542ea7ae3540288ce86cb8a6d0601ae2
Shinzo SAITO <architshin@websarva.com>
2021-11-14 04:17:14 +0900

    components-props-validationの追加。

---
+++ chap08/components-props-validation/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+	<section>
+		<h1>会員リスト</h1>
+		<p>全会員の保有ポイントの合計: {{totalPoints}}</p>
+		<OneMember
+			v-for="[id, member] in memberList"
+			v-bind:key="id"
+			v-bind:id="id"
+			v-bind:name="member.name"
+			v-bind:email="member.email"
+			v-bind:points="member.points"
+			v-bind:note="member.note"/>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, computed} from "vue";
+import OneMember from "./components/OneMember.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		OneMember
+	},
+	setup() {
+		const memberListInit = new Map<number, Member>();
+		memberListInit.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "ちょ〜イケメン。"});
+		memberListInit.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});
+		// memberListInit.set(47785, {id: 47785, points: 80});
+		// memberListInit.set(47785, {id: 47785, points: -10});
+		const memberList = ref(memberListInit);
+
+		const totalPoints = computed(
+			(): number => {
+				let total = 0;
+				for(const [id, member] of memberList.value) {
+					total += member.points;
+				}
+				return total;
+			}
+		);
+		return {
+			memberList,
+			totalPoints
+		}
+	}
+});
+
+interface Member {
+	id: number;
+	name: string;
+	email: string;
+	points: number;
+	note?: string;
+}
+// interface Member {
+// 	id: number;
+// 	name?: string;
+// 	email?: string;
+// 	points: number;
+// 	note?: string;
+// }
+</script>
