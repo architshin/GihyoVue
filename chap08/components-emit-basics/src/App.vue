ded040fdbb5478b4335a78fbb6f41347e2adade3
Shinzo SAITO <architshin@websarva.com>
2021-11-15 01:24:07 +0900

    components-emit-basicsの追加。

---
+++ chap08/components-emit-basics/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<section>
+		<p>親コンポーネントで乱数を表示: {{rand}}</p>
+		<OneSection
+			v-bind:rand="rand"
+			v-on:createNewRand="onCreateNewRand"/>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		OneSection
+	},
+	setup() {
+		const randInit = Math.round(Math.random() * 10);
+		const rand = ref(randInit);
+		const onCreateNewRand = (): void => {
+			rand.value = Math.round(Math.random() * 10);
+		}
+		return {
+			rand,
+			onCreateNewRand
+		}
+	}
+});
+</script>
