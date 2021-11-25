9643a16bf0748ce9aba5f49bfe782eff820ddd44
Shinzo SAITO <architshin@websarva.com>
2021-11-26 00:20:32 +0900

    components-props-tagプロジェクトの追加。

---
+++ chap09/components-props-tag/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<section>
+		<h2>単純なpタグの埋め込み</h2>
+		<OneSection v-bind:tag="tag"/>
+	</section>
+	<section>
+		<h2>複雑なタグの埋め込み</h2>
+		<OneSection v-bind:tag="tagComplex"/>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		OneSection
+	},
+	setup() {
+		const tag = ref("<p>Appで用意したpタグ</p>");
+		const tagComplex = ref(`<ul><li v-for="problem in problems" v-bind:key="problem">{{problem}}</li></ul>`);
+		return {
+			tag,
+			tagComplex
+		}
+	}
+});
+</script>
---
+++ chap09/components-props-tag/src/components/OneSection.vue
@@ -0,0 +1,25 @@
+<template>
+	<section class="box">
+		{{tag}}
+	</section>
+	<section class="box" v-html="tag">
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+	name: "OneSection",
+	props: {
+		tag: String
+	}
+});
+</script>
+
+<style>
+.box {
+	border: green 1px solid;
+	margin: 10px;
+}
+</style>
