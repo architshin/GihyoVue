f4d8d95c84897090a95c7abd96157cff684c9dcf
Shinzo SAITO <architshin@websarva.com>
2021-11-26 21:16:38 +0900

    slot-datascopeプロジェクトの追加。

---
+++ chap09/slot-datascope/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<section>
+		<h2>Slotの利用</h2>
+		<OneSection v-bind:name="taro">
+			<p>{{taro}}さんは連絡がつきません。</p>
+			<!-- <p>{{name}}さんは連絡がつきません。</p> -->
+		</OneSection>
+		<OneSection v-bind:name="jiro"/>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		OneSection
+	},
+	setup() {
+		const taro = ref("田中太郎");
+		const jiro = ref("鈴木二郎");
+		return {
+			taro,
+			jiro
+		}
+	}
+});
+</script>
---
+++ chap09/slot-datascope/src/components/OneSection.vue
@@ -0,0 +1,27 @@
+<template>
+	<section class="box">
+		<h1>{{name}}さんの状況</h1>
+		<slot>
+			<p>{{name}}さんは問題ありません。</p>
+			<!-- <p>{{jiro}}さんは問題ありません。</p> -->
+		</slot>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+	name: "OneSection",
+	props: {
+		name: String
+	}
+});
+</script>
+
+<style>
+.box {
+	border: green 1px solid;
+	margin: 10px;
+}
+</style>
