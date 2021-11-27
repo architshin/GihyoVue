6dbb98056a26c4fe4d362ac8622f93b08404b1ce
Shinzo SAITO <architshin@websarva.com>
2021-11-27 19:10:18 +0900

    slot-dynamicプロジェクトの追加。

---
+++ chap09/slot-dynamic/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<section>
+		<OneSection v-bind:name="taro">
+			<ul>
+				<li v-for="problem in taroProblems" v-bind:key="problem">
+					{{problem}}
+				</li>
+			</ul>
+		</OneSection>
+		<OneSection v-bind:name="jiro"/>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		OneSection
+	},
+	setup() {
+		const taroProblemsInit: string[] = ["電話が通じません。", "留守です。"];
+		const taro = ref("田中太郎");
+		const taroProblems = ref(taroProblemsInit);
+		const jiro = ref("鈴木二郎");
+		return {
+			taro,
+			taroProblems,
+			jiro
+		}
+	}
+});
+</script>
