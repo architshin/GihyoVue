47a632cf0ae8407b6ec3c06591d21ebf6721a701
Shinzo SAITO <architshin@websarva.com>
2021-11-28 19:24:30 +0900

    slot-scopedプロジェクトの追加。

---
+++ chap09/slot-scoped/src/components/OneSection.vue
@@ -0,0 +1,26 @@
+<template>
+	<section>
+		<slot v-bind:memberInfo="memberInfo">
+		<!-- <slot v-bind:memberName="memberInfo.name" v-bind:memberState="memberInfo.state"> -->
+			<h1>{{memberInfo.name}}さんの状況</h1>
+			<p>{{memberInfo.state}}</p>
+		</slot>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent, reactive} from "vue";
+
+export default defineComponent({
+	name: "OneSection",
+	setup() {
+		const memberInfo = reactive({
+			name: "田中太郎",
+			state: "問題ありません。"
+		});
+		return {
+			memberInfo
+		}
+	}
+});
+</script>
