761272a6409477916c909af97cdd351d1099496f
Shinzo SAITO <architshin@websarva.com>
2021-12-12 16:22:40 +0900

    router-paramsプロジェクトの追加。

---
+++ chap10/router-params/src/router/index.ts
@@ -0,0 +1,52 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+import Top from "../views/Top.vue";
+
+const routes: Array<RouteRecordRaw> = [
+	{
+		path: "/",
+		name: "Top",
+		component: Top
+	},
+	{
+		path: "/member/search/:name/:points",
+		name: "SeveralParams",
+		component: () => {
+			return import("../views/ParamsViewer.vue");
+		}
+	},
+	{
+		path: "/member/show/:name/:points?",
+		name: "OptionalParams",
+		component: () => {
+			return import("../views/ParamsViewer.vue");
+		}
+	},
+	{
+		path: "/member/call/:id*",
+		name: "Repeatable0Params",
+		component: () => {
+			return import("../views/ParamsViewer.vue");
+		}
+	},
+	{
+		path: "/member/tell/:id+",
+		name: "Repeatable1Params",
+		component: () => {
+			return import("../views/ParamsViewer.vue");
+		}
+	},
+	{
+		path: "/member/pull/:id(\\d{5})",
+		name: "RegexpParams",
+		component: () => {
+			return import("../views/ParamsViewer.vue");
+		}
+	}
+]
+
+const router = createRouter({
+	history: createWebHistory(process.env.BASE_URL),
+	routes
+})
+
+export default router
---
+++ chap10/router-params/src/views/ParamsViewer.vue
@@ -0,0 +1,31 @@
+<template>
+	<h1>会員管理</h1>
+	<nav id="breadcrumbs">
+		<ul>
+			<li>
+				<router-link v-bind:to="{name: 'Top'}">TOP</router-link>
+			</li>
+			<li>ルートパラメータ参照</li>
+		</ul>
+	</nav>
+	<section>
+		<h2>ルートパラメータ参照</h2>
+		<p>{{params}}</p>
+	</section>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {useRoute} from "vue-router";
+
+export default defineComponent({
+	name: "ParamsViewer",
+	setup() {
+		const route = useRoute();
+		const params = route.params;
+		return {
+			params
+		};
+	}
+});
+</script>
