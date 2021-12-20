e309189b47d73b1908913edd692af9cfa9183a5c
Shinzo SAITO <architshin@websarva.com>
2021-12-20 21:11:27 +0900

    vuex-basicプロジェクトが一応完成。

---
+++ chap11/vuex-basic/src/router/index.ts
@@ -0,0 +1,44 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+import AppTop from "../views/AppTop.vue";
+
+const routes: Array<RouteRecordRaw> = [
+	{
+		path: "/",
+		name: "AppTop",
+		component: AppTop
+	},
+	{
+		path: "/member/memberList",
+		name: "MemberList",
+		component: () => {
+			return import("../views/member/MemberList.vue");
+		}
+	},
+	{
+		path: "/member/detail/:id",
+		name: "MemberDetail",
+		component: () => {
+			return import("../views/member/MemberDetail.vue");
+		},
+		props: (routes) => {
+			const idNum = Number(routes.params.id);
+			return {
+				id: idNum
+			};
+		}
+	},
+	{
+		path: "/member/add",
+		name: "MemberAdd",
+		component: () => {
+			return import("../views/member/MemberAdd.vue");
+		}
+	}
+]
+
+const router = createRouter({
+	history: createWebHistory(process.env.BASE_URL),
+	routes
+})
+
+export default router
---
+++ chap11/vuex-basic/src/store/index.ts
@@ -0,0 +1,29 @@
+import { createStore } from "vuex";
+import {Member} from "../interfaces";
+
+export default createStore({
+	state: {
+		memberList: new Map<number, Member>()
+	},
+	getters: {
+		getById: (state) => {
+			return (id: number): Member => {
+				const member = state.memberList.get(id) as Member;
+				return member;
+			}
+		}
+	},
+	mutations: {
+		INIT_LIST(state): void {
+			state.memberList.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "ちょ〜イケメン。"});
+			state.memberList.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});	
+		},
+		ADD_MEMBER(state, member: Member): void {
+			state.memberList.set(member.id, member);
+		}
+	},
+	actions: {
+	},
+	modules: {
+	}
+})
