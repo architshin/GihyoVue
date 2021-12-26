5e6d86ea2b8d7a0e649eb81a35a86f7ecde8758c
Shinzo SAITO <architshin@websarva.com>
2021-12-26 19:32:07 +0900

    vuex-fundamental-storageを原稿に合わせて変更。

--- chap11/vuex-fundamental-storage/src/store/index.ts
+++ chap11/vuex-fundamental-storage/src/store/index.ts
@@ -3,8 +3,8 @@ import {createStore, Store, useStore as baseUseStore} from "vuex";
 import {Member} from "@/interfaces";
 
 export enum MutationsList {
-	ADD_MEMBER = "addMember",
-	CHANGE_LIST = "changeList"
+	CHANGE_LIST = "changeList",
+	ADD_MEMBER = "addMember"
 }
 
 export enum ActionsList {
@@ -35,27 +35,36 @@ export default createStore<State>({
 		}
 	},
 	mutations: {
-		[MutationsList.ADD_MEMBER](state, member: Member): void {
-			state.memberList.set(member.id, member);
-		},
 		[MutationsList.CHANGE_LIST](state, memberList: Map<number, Member>): void {
 			state.memberList = memberList;
+		},
+		[MutationsList.ADD_MEMBER](state, member: Member): void {
+			state.memberList.set(member.id, member);
 		}
 	},
 	actions: {
 		[ActionsList.PREPARE_MEMBER_LIST](context): void {
+			//空のmemberListを用意。
 			let memberList = new Map<number, Member>();
+			//セッションストレージからデータを取得。
 			const memberListJSONStr = sessionStorage.getItem("memberList");
+			//セッションストレージのデータが空じゃないなら
 			if(memberListJSONStr != undefined) {
+				//JSON文字列をJSONオブジェクトに変換。
 				const memberListJSON = JSON.parse(memberListJSONStr);
+				//JSONオブジェクトをもとにmemberListを生成。
 				memberList = new Map<number, Member>(memberListJSON);
 				// console.log(`CHANGE_LIST: ${memberList}`);
 			}
+			//ステートにmemberListを格納。
 			context.commit(MutationsList.CHANGE_LIST, memberList);
 		},
 		[ActionsList.INSERT_MEMBER](context, member: Member): void {
+			//ステートのmemberListに引数の会員情報を追加。
 			context.commit(MutationsList.ADD_MEMBER, member);
+			//ステートのmemberListをJSON文字列に変換。
 			const memberListJSONStr = JSON.stringify([...context.state.memberList]);
+			//セッションストレージに格納。
 			sessionStorage.setItem("memberList", memberListJSONStr);
 			// console.log(`INSERT_MEMBER: ${memberListJSONStr}`);
 		}
