ea7adc74c6cd8f70c7a2cd92e2543466bbb04a9d
Shinzo SAITO <architshin@websarva.com>
2022-01-07 16:05:49 +0900

    utest-inputプロジェクトの追加。

---
+++ chap12/utest-input/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<p>
+		<input type="number" id="num1" v-model="num1">
+		÷
+		<input type="number" id="num2" v-model="num2">
+		=<span id="ans">{{ans}}</span>
+	</p>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, computed} from "vue";
+
+export default defineComponent({
+	name: "App",
+	setup() {
+		const num1 = ref(6);
+		const num2 = ref(3);
+		const ans = computed(
+			(): number => {
+				return num1.value / num2.value;
+			}
+		);
+		return {
+			num1,
+			num2,
+			ans
+		}
+	}
+});
+</script>
