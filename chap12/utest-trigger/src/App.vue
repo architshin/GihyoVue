233a97527eeb239e554ad247673572334827aab7
Shinzo SAITO <architshin@websarva.com>
2022-01-26 13:31:50 +0900

    第12章のサンプル追加。 (#9)
    * utest-basicプロジェクトを追加。
    
    * utest-basicにコンポーネントテストを追加。
    
    * utest-basicの利用メソッドを変更。
    
    * utest-inputプロジェクトの追加。
    
    * utest-triggerプロジェクトの追加。
    
    * utest-propsプロジェクトの追加。
    
    * テストファイルをTSに変更。さらにdata-testidを導入。
    
    * utest-propsにテストを追加。
    
    * utest-propsプロジェクトのApp.vueのテストを拡充。
    
    * utest-propsプロジェクトの名称をutest-componentsに変更。
    
    * utest-basicプロジェクトを原稿に合わせて変更。
    
    * utest-triggerにv-showの検証コードを追加。
    
    * utest-componentsのOneMember.spec.tsを原稿に合わせて変更。
    
    * utest-componentsを微修正。
    
    * utest-componentsのマウントオプション記述の書式を変更。

---
+++ chap12/utest-trigger/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<p>
+		隠し領域を
+		<button data-testid="showButton" v-on:click="onShowButtonClick">表示</button>
+	</p>
+	<p v-if="isVisible" data-testid="invisible">表示されました!</p>
+	<p v-show="isVisible" data-testid="invisible2">表示されました!</p>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+	name: "App",
+	setup() {
+		const isVisible = ref(false);
+		const onShowButtonClick = (): void => {
+			isVisible.value = true;
+		};
+		return {
+			isVisible,
+			onShowButtonClick
+		}
+	}
+});
+</script>
