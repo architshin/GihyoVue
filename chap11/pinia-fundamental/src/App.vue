9a38c00444cdc0857b338b76078e7af8671c4a44
Shinzo SAITO <architshin@websarva.com>
2022-02-21 18:49:52 +0900

    pinia-fundamentalの追加。

---
+++ chap11/pinia-fundamental/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {RouterView} from "vue-router";
+import {useMembersStore} from "@/stores/members";
+
+const membersStore = useMembersStore();
+membersStore.initList();
+</script>
+
+<template>
+	<header>
+		<h1>Piniaサンプル</h1>
+	</header>
+	<main>
+		<RouterView/>
+	</main>
+</template>
+
+<style>
+main {
+	border: blue 1px solid;
+	padding: 10px;
+}
+#breadcrumbs ul li {
+	display: inline;
+	list-style-type: none;
+}
+#breadcrumbs {
+	margin-left: 0px;
+}
+#breadcrumbs ul {
+	padding-left: 0px;
+}
+#breadcrumbs ul .current {
+	color: red;
+}
+#breadcrumbs ul li:before {
+	content: " > ";
+}
+#breadcrumbs ul li:first-child:before {
+	content: none;
+}
+</style>
---
+++ chap11/pinia-fundamental/src/stores/members.ts
@@ -0,0 +1,32 @@
+import {defineStore} from "pinia";
+import type {Member} from "@/interfaces";
+
+interface State {
+	memberList: Map<number, Member>
+}
+
+export const useMembersStore = defineStore({
+	id: "members",
+	state: (): State => {
+		return {
+			memberList: new Map<number, Member>()
+		}
+	},
+	getters: {
+		getById: (state) => {
+			return (id: number): Member => {
+				const member = state.memberList.get(id) as Member;
+				return member;
+			}
+		}
+	},
+	actions: {
+		initList(): void {
+			this.memberList.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "ちょ〜イケメン。"});
+			this.memberList.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});	
+		},
+		addMember(member: Member): void {
+			this.memberList.set(member.id, member);
+		}
+	}
+});
