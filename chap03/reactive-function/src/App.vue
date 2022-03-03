bbba71398ec4302001897be1a727ae2abe1af572
Shinzo SAITO <architshin@websarva.com>
2022-03-04 02:48:58 +0900

    reactive-functionをVite環境に変更。

--- chap03/reactive-function/src/App.vue
+++ chap03/reactive-function/src/App.vue
@@ -1,37 +1,26 @@
+<script setup lang="ts">
+import {reactive, computed} from "vue";
+
+// リアクティブなテンプレート変数をまとめて用意。
+const data = reactive({
+	PI: 3.14,
+	radius: Math.round(Math.random() * 10)
+});
+// 円の面積の算出値を用意。
+const area = computed(
+	(): number => {
+		return data.radius * data.radius * data.PI;
+	}
+);
+// 半径のテンプレート変数に新しい乱数を1秒ごとに格納。
+setInterval(
+	():void => {
+		data.radius = Math.round(Math.random() * 10);
+	},
+	1000
+);
+</script>
+
 <template>
 	<p>半径{{data.radius}}の円の面積を円周率{{data.PI}}で計算すると、{{area}}</p>
 </template>
-
-<script lang="ts">
-import {defineComponent, reactive, computed} from "vue";
-
-export default defineComponent({
-	setup() {
-		// リアクティブなテンプレート変数をまとめて用意。
-		const data = reactive({
-			PI: 3.14,
-			radius: Math.round(Math.random() * 10)
-		});
-		// 円の面積の算出値を用意。
-		const area = computed(
-			function() {
-				return data.radius * data.radius * data.PI;
-			}
-		);
-		// 半径のテンプレート変数に新しい乱数を格納する関数。
-		function changeRadius() {
-			data.radius = Math.round(Math.random() * 10);
-		}
-		// changeRadius()を1秒ごとに実行。
-		setInterval(changeRadius, 1000);
-		// テンプレート変数をリターン。
-		return {
-			data,
-			area
-		};
-	}
-});
-</script>
-
-<style>
-</style>
