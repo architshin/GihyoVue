0c68d50f8ad35b3285bdf152c8daa11b441bd97e
Shinzo SAITO <architshin@websarva.com>
2022-03-11 14:52:54 +0900

    第3章を新構成に合わせて変更。 (#14)
    * mustacheをVite環境に変更。
    
    * reactive-systemをVite環境に変更。
    
    * computedをVite環境に変更。
    
    * reactive-functionをVite環境に変更。
    
    * 不要なtorefsを削除。
    
    * 戻り値を記述。

--- chap03/computed/src/App.vue
+++ chap03/computed/src/App.vue
@@ -1,48 +1,36 @@
+<script setup lang="ts">
+import {ref, computed} from "vue";
+
+// 半径の初期値を乱数で取得。
+const radiusInit = Math.round(Math.random() * 10);
+// 円周率のテンプレート変数を用意。
+const PI = ref(3.14);
+// 半径のテンプレート変数を用意。
+const radius = ref(radiusInit);
+// 円の面積の算出値を用意。
+const area = computed(
+	(): number => {
+		return radius.value * radius.value * PI.value;
+	}
+);
+// const area = computed(
+// 	(): number => radius.value * radius.value * PI.value
+// );
+// const area = computed(
+// 	function(): number {
+// 		return radius.value * radius.value * PI.value;
+// 	}
+// );
+// 半径のテンプレート変数に新しい乱数を1秒ごとに格納。
+setInterval(
+	():void => {
+		radius.value = Math.round(Math.random() * 10);
+	},
+	1000
+);
+// area.value = 300;
+</script>
+
 <template>
 	<p>半径{{radius}}の円の面積を円周率{{PI}}で計算すると、{{area}}</p>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref, computed} from "vue";
-
-export default defineComponent({
-	setup() {
-		// 半径の初期値を乱数で取得。
-		const radiusInit = Math.round(Math.random() * 10);
-		// 円周率のテンプレート変数を用意。
-		const PI = ref(3.14);
-		// 半径のテンプレート変数を用意。
-		const radius = ref(radiusInit);
-		// 円の面積の算出値を用意。
-		const area = computed(
-			function() {
-				return radius.value * radius.value * PI.value;
-			}
-		);
-		// const area = computed(
-		// 	() => {
-		// 		return radius.value * radius.value * PI;
-		// 	}
-		// );
-		// const area = computed(
-		// 	() => radius.value * radius.value * PI
-		// );
-		// 半径のテンプレート変数に新しい乱数を格納する関数。
-		function changeRadius() {
-			radius.value = Math.round(Math.random() * 10);
-		}
-		// changeRadius()を1秒ごとに実行。
-		setInterval(changeRadius, 1000);
-		// area.value = 300;
-		// テンプレート変数をリターン。
-		return {
-			radius,
-			PI,
-			area
-		};
-	}
-});
-</script>
-
-<style>
-</style>
--- chap03/mustache/src/App.vue
+++ chap03/mustache/src/App.vue
@@ -1,21 +1,11 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const name = ref("田中太郎");
+// const yourName = "田中太郎";
+// const name = ref(yourName);
+</script>
+
 <template>
 	<h1>こんにちは!{{name}}さん!</h1>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const name = ref("田中太郎");
-		// const yourName = "田中太郎";
-		// const name = ref(yourName);
-		return {
-			name
-		};
-	}
-});
-</script>
-
-<style>
-</style>
