96dee63199af30cebea0be69249d3901e53eadc3
Shinzo SAITO <architshin@websarva.com>
2022-03-12 02:02:36 +0900

    watchをVite環境に合わせて更新。

--- chap07/watch/src/App.vue
+++ chap07/watch/src/App.vue
@@ -1,54 +1,54 @@
-<template>
-	<select v-model.number="selectedCocktailId">
-		<option value="0">未選択</option>
-		<option value="2345">ホワイトレディ</option>
-		<option value="4412">ブルーハワイ</option>
-		<option value="6792">ニューヨーク</option>
-		<option value="8429">マティーニ</option>
-	</select>
-	<p>{{priceMsg}}</p>
-</template>
+<script setup lang="ts">
+import {ref, watch} from "vue";
 
-<script lang="ts">
-import {defineComponent, ref, watch} from "vue";
-
-export default defineComponent({
-	setup() {
-		//ドロップダウンリストで選択されたvalue値とリアクティブな変数を用意。
-		const selectedCocktailId = ref(0);
-		//選択されたカクテル情報を表示するリアクティブな変数を用意。
-		const priceMsg = ref("");
-		//watchを設定。
-		watch(selectedCocktailId,
-			(): void => {
-				//選択されたカクテルデータを取得。
-				const cocktailDataListInit = new Map<number, Cocktail>();
-				cocktailDataListInit.set(2345, {id: 2345, name: "ホワイトレディ", price: 1200});
-				cocktailDataListInit.set(4412, {id: 4412, name: "ブルーハワイ", price: 1500});
-				cocktailDataListInit.set(6792, {id: 6792, name: "ニューヨーク", price: 1100});
-				cocktailDataListInit.set(8429, {id: 8429, name: "マティーニ", price: 1500});
-				const cocktail = cocktailDataListInit.get(selectedCocktailId.value);
-				//未選択な状態のメッセージを用意。
-				let msg = "カクテルは未選択です。";
-				//選択カクテルデータが存在しているなら…
-				if(cocktail != undefined) {
-					//選択されたカクテルの名前と金額を表示する文字列を生成。
-					msg = `選択されたカクテルは${cocktail.name}で、価格は${cocktail.price}円です。`;
-				}
-				//表示文字列をpriceMsgに設定。
-				priceMsg.value = msg;
-			}
-		);
-		return {
-			selectedCocktailId,
-			priceMsg
-		};
+//カクテル番号のテンプレート変数を用意。
+const cocktailNo = ref(1);
+//カクテル番号に対応するカクテル情報の産出プロパティを用意。
+const priceMsg = ref("");
+//watchを設定。
+watch(cocktailNo,
+	(): void => {
+		priceMsg.value = getCocktailInfo(cocktailNo.value);
 	}
-});
+);
+
+//cocktailNoを1秒ごとに1〜4の乱数を使って変更。
+setInterval(
+	():void => {
+		cocktailNo.value = Math.round(Math.random() * 3) + 1;
+	},
+	1000
+);
 
 interface Cocktail {
 	id: number;
 	name: string;
 	price: number;
 }
+
+//カクテル番号に対応するカクテル情報を取得する関数。
+function getCocktailInfo(cocktailNo: number): string {
+	//カクテルリストデータを用意。
+	const cocktailDataListInit = new Map<number, Cocktail>();
+	cocktailDataListInit.set(1, {id: 1, name: "ホワイトレディ", price: 1200});
+	cocktailDataListInit.set(2, {id: 2, name: "ブルーハワイ", price: 1500});
+	cocktailDataListInit.set(3, {id: 3, name: "ニューヨーク", price: 1100});
+	cocktailDataListInit.set(4, {id: 4, name: "マティーニ", price: 1500});
+	//カクテル番号に該当するカクテルデータを取得。
+	const cocktail = cocktailDataListInit.get(cocktailNo);
+	//カクテル番号に該当する情報がない場合のメッセージを用意。
+	let msg = "該当カクテルはありません。";
+	//カクテル番号に該当する情報があるなら…
+	if(cocktail != undefined) {
+		//カクテル番号に該当するカクテルの名前と金額を表示する文字列を生成。
+		msg = `該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円です。`;
+	}
+	//表示文字列をリターン。
+	return msg;
+}
 </script>
+
+<template>
+	<p>現在のカクテル番号: {{cocktailNo}}</p>
+	<p>{{priceMsg}}</p>
+</template>
