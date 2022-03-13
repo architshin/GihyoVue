b329b54ff6132eb550d185d8ac001f3073be3d2a
Shinzo SAITO <architshin@websarva.com>
2022-03-14 00:09:20 +0900

    components-props-vforをVite環境に合わせて更新。

--- chap08/components-props-vfor/src/App.vue
+++ chap08/components-props-vfor/src/App.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import OneInfo from "./components/OneInfo.vue";
+
+const weatherListInit = new Map<number, Weather>();
+weatherListInit.set(1, {id: 1, title: "今日の天気", content: "今日は一日中、晴でしょう。"});
+weatherListInit.set(2, {id: 2, title: "明日の天気", content: "明日は一日中、雨でしょう。"});
+weatherListInit.set(3, {id: 3, title: "明後日の天気", content: "明後日は一日中、雪でしょう。"});
+const weatherList = ref(weatherListInit);
+
+interface Weather {
+	id: number;
+	title: string;
+	content: string;
+}
+</script>
+
 <template>
-	<h1>Props基礎</h1>
+	<h1>プロップス基礎</h1>
 	<section>
 		<h2>ループでコンポーネントを生成</h2>
 		<OneInfo
@@ -9,31 +26,3 @@
 			v-bind:content="weather.content"/>
 	</section>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-import OneInfo from "./components/OneInfo.vue";
-
-export default defineComponent({
-	name: "App",
-	components: {
-		OneInfo
-	},
-	setup() {
-		const weatherListInit = new Map<number, Weather>();
-		weatherListInit.set(1, {id: 1, title: "今日の天気", content: "今日は一日中、晴でしょう。"});
-		weatherListInit.set(2, {id: 2, title: "明日の天気", content: "明日は一日中、雨でしょう。"});
-		weatherListInit.set(3, {id: 3, title: "明後日の天気", content: "明後日は一日中、雪でしょう。"});
-		const weatherList = ref(weatherListInit);
-		return {
-			weatherList
-		}
-	}
-});
-
-interface Weather {
-	id: number;
-	title: string;
-	content: string;
-}
-</script>
