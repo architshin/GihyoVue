8da8b40655704b2a4a1cd18a2066052b80eea857
Shinzo SAITO <architshin@websarva.com>
2022-03-14 02:02:06 +0900

    components-props-indepthの追加。

---
+++ chap08/components-props-indepth/src/components/OneMember.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import {ref, computed} from "vue";
+
+//プロップスインターフェースの定義。
+interface Props {
+	id: number;
+	name: string;
+	email: string;
+	points: number;
+	note?: string;
+}
+//プロップスオブジェクトの設定。
+const props = defineProps<Props>();
+
+//このコンポーネント内で利用するポイント数のテンプレート変数。
+const localPoints = ref(props.points);
+//noteプロップスを加工する算出プロパティ。
+const localNote = computed(
+	(): string => {
+		let localNote = props.note;
+		if(localNote == undefined) {
+			localNote = "--";
+		}
+		return localNote;
+	}
+);
+//［ポイント加算］ボタンをクリックした時のメソッド。
+const pointUp = (): void => {
+	// props.points++;
+	localPoints.value++;
+}
+</script>
+
+<template>
+	<section class="box">
+		<h4>{{name}}さんの情報</h4>
+		<dl>
+			<dt>ID</dt>
+			<dd>{{id}}</dd>
+			<dt>メールアドレス</dt>
+			<dd>{{email}}</dd>
+			<dt>保有ポイント</dt>
+			<dd>{{localPoints}}</dd>
+			<dt>備考</dt>
+			<dd>{{localNote}}</dd>
+		</dl>
+		<button v-on:click="pointUp">ポイント加算</button>
+	</section>
+</template>
+
+<style scoped>
+.box {
+	border: green 1px solid;
+	margin: 10px;
+}
+</style>
