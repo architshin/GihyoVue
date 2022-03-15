96018217f31d6ce07d25b5b4a04446c335947786
Shinzo SAITO <architshin@websarva.com>
2022-03-16 01:55:15 +0900

    slot-datascopeをVite環境に合わせて更新。

--- chap09/slot-datascope/src/App.vue
+++ chap09/slot-datascope/src/App.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+const taro = ref("田中太郎");
+const jiro = ref("鈴木二郎");
+</script>
+
 <template>
 	<section>
-		<h2>Slotの利用</h2>
+		<h2>スロットの利用</h2>
 		<OneSection v-bind:name="taro">
 			<p>{{taro}}さんは連絡がつきません。</p>
 			<!-- <p>{{name}}さんは連絡がつきません。</p> -->
@@ -8,23 +16,3 @@
 		<OneSection v-bind:name="jiro"/>
 	</section>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-import OneSection from "./components/OneSection.vue";
-
-export default defineComponent({
-	name: "App",
-	components: {
-		OneSection
-	},
-	setup() {
-		const taro = ref("田中太郎");
-		const jiro = ref("鈴木二郎");
-		return {
-			taro,
-			jiro
-		}
-	}
-});
-</script>
