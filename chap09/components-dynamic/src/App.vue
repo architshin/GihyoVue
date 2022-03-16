c2218fa6987d3e97cf237e4ab38082840361c74e
Shinzo SAITO <architshin@websarva.com>
2022-03-16 20:05:18 +0900

    components-dynamicをVite環境に合わせて更新。

--- chap09/components-dynamic/src/App.vue
+++ chap09/components-dynamic/src/App.vue
@@ -1,47 +1,38 @@
-<template>
-	<p>コンポーネント名: {{currentComp}}</p>
-	<keep-alive>
-		<component v-bind:is="currentComp"/>
-	</keep-alive>
-	<button v-on:click="switchComp">切り替え</button>
-</template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
+<script setup lang="ts">
+import {ref} from "vue";
 import Input from "./components/Input.vue";
 import Radio from "./components/Radio.vue";
 import Select from "./components/Select.vue";
 
-export default defineComponent({
-	name: "App",
-	components: {
-		Input,
-		Radio,
-		Select
-	},
-	setup() {
-		//現在表示させるコンポーネント名を表すテンプレート変数。
-		const currentComp = ref("Input");
-		//コンポーネント名の配列。
-		const compList: string[] = ["Input", "Radio", "Select"];
-		//現在表示させているコンポーネントに対応した配列のインデックス番号。
-		let currentCompIndex = 0;
-		//コンポーネントを切り替えるメソッド。
-		const switchComp = (): void => {
-			//インデックス番号をインクリメント。
-			currentCompIndex++;
-			//インデックス番号が3以上なら…
-			if(currentCompIndex >= 3) {
-				//0にリセット。
-				currentCompIndex = 0;
-			}
-			//インデックス番号に該当するテンプレート名をcurrentCompに代入。
-			currentComp.value = compList[currentCompIndex];
-		}
-		return {
-			currentComp,
-			switchComp
-		}
+//現在表示させるコンポーネントを表すテンプレート変数。
+const currentComp = ref(Input);
+//現在表示させるコンポーネント名を表すテンプレート変数。
+const currentCompName = ref("Input");
+//コンポーネントの配列。
+const compList = [Input, Radio, Select];
+const compNameList: string[] = ["Input", "Radio", "Select"];
+//現在表示させているコンポーネントに対応した配列のインデックス番号。
+let currentCompIndex = 0;
+//コンポーネントを切り替えるメソッド。
+const switchComp = (): void => {
+	//インデックス番号をインクリメント。
+	currentCompIndex++;
+	//インデックス番号が3以上なら…
+	if(currentCompIndex >= 3) {
+		//0にリセット。
+		currentCompIndex = 0;
 	}
-});
+	//インデックス番号に該当するコンポーネントをcurrentCompに代入。
+	currentComp.value = compList[currentCompIndex];
+	//インデックス番号に該当するコンポーネント名をcurrentCompNameに代入。
+	currentCompName.value = compNameList[currentCompIndex];
+}
 </script>
+
+<template>
+	<p>コンポーネント名: {{currentCompName}}</p>
+	<KeepAlive>
+		<component v-bind:is="currentComp"/>
+	</KeepAlive>
+	<button v-on:click="switchComp">切り替え</button>
+</template>
