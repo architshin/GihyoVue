86c71f013b7c52f54563dfefdbcf345eefc3a368
Shinzo SAITO <architshin@websarva.com>
2022-03-18 03:42:44 +0900

    directive-on-param-othersをVite環境に合わせて更新。

--- chap04/directive-on-param-others/src/App.vue
+++ chap04/directive-on-param-others/src/App.vue
@@ -1,22 +1,14 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const pBgColor = ref("white");
+const onPClick = (bgColor: string): void => {
+	pBgColor.value = bgColor;
+};
+</script>
+
 <template>
 	<p v-on:click="onPClick('red')" v-bind:style="{backgroundColor: pBgColor}">
 		ここをクリックすると背景色が変わります。
 	</p>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const pBgColor = ref("white");
-		const onPClick = function(bgColor: string): void {
-			pBgColor.value = bgColor;
-		};
-		return {
-			pBgColor,
-			onPClick
-		};
-	}
-});
-</script>
