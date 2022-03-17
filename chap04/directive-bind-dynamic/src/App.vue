7e841e44c9afd927b2940efc4c92cf7d7e2e073b
Shinzo SAITO <architshin@websarva.com>
2022-03-18 01:35:36 +0900

    directive-bind-dynamicをVite環境に合わせて更新。

--- chap04/directive-bind-dynamic/src/App.vue
+++ chap04/directive-bind-dynamic/src/App.vue
@@ -1,18 +1,10 @@
-<template>
-	<p><img alt="VueLogo" src="./assets/logo.png" v-bind:[widthOrHeight]="widthOrHeightValue"></p>
-</template>
+<script setup lang="ts">
+import {ref} from "vue";
 
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const widthOrHeight = ref("height");
-		const widthOrHeightValue = ref(100);
-		return {
-			widthOrHeight,
-			widthOrHeightValue
-		};
-	}
-});
+const widthOrHeight = ref("height");
+const widthOrHeightValue = ref(100);
 </script>
+
+<template>
+	<p><img alt="VueLogo" src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue"></p>
+</template>
