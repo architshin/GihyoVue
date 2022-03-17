6995c65465e0ee7c451aaa3126caa42fe8cda037
Shinzo SAITO <architshin@websarva.com>
2022-03-18 02:18:54 +0900

    directive-bind-objectをVite環境に合わせて更新。

--- chap04/directive-bind-object/src/App.vue
+++ chap04/directive-bind-object/src/App.vue
@@ -1,23 +1,17 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const imgAttributes = ref({
+	src: "/images/logo.svg",
+	// src: "./assets/logo.svg",
+	alt: "Vueのロゴ",
+	width: 75,
+	height: 75
+});
+</script>
+
 <template>
 	<p><img v-bind="imgAttributes"></p>
 	<p><img v-bind="imgAttributes" title="ロゴです!"></p>
 	<p><img v-bind="imgAttributes" alt="ロゴです!"></p>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const imgAttributes = ref({
-			src: "/images/logo.png",
-			alt: "Vueのロゴ",
-			width: 75,
-			height: 75
-		});
-		return {
-			imgAttributes
-		};
-	}
-});
-</script>
