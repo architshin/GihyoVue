8f7309e5f9257e7bca18690f1dd8cb74e329a2d6
Shinzo SAITO <architshin@websarva.com>
2022-03-18 20:13:44 +0900

    第5章を新構成に合わせて変更。 (#20)
    * directive-modelをVite環境に合わせて更新。
    
    * directive-onewayをVite環境に合わせて更新。
    
    * directive-twowayをVite環境に合わせて更新。
    
    * directive-model-controlsをVite環境に合わせて更新。
    
    * directive-model-trimをVite環境に合わせて更新。
    
    * directive-htmlをVite環境に合わせて更新。
    
    * directive-preをVite環境に合わせて更新。
    
    * directive-onceをVite環境に合わせて更新。
    
    * directive-cloakをVite環境に合わせて更新。

--- chap05/directive-model-trim/src/App.vue
+++ chap05/directive-model-trim/src/App.vue
@@ -1,19 +1,12 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const trimedText = ref("");
+</script>
+
 <template>
 	<section>
 		<input type="text" v-model.trim="trimedText">
 		<p>入力文字列: {{trimedText}}</p>
 	</section>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const trimedText = ref("");
-		return {
-			trimedText
-		};
-	}
-});
-</script>
--- chap05/directive-pre/src/App.vue
+++ chap05/directive-pre/src/App.vue
@@ -3,10 +3,3 @@
 		<p v-on:click="showHello">{{hello!}}</p>
 	</section>
 </template>
-
-<script lang="ts">
-import {defineComponent} from "vue";
-
-export default defineComponent({
-});
-</script>
