336b486557e5910cbd14f690c7189acbce49ca13
Shinzo SAITO <architshin@websarva.com>
2022-03-18 18:51:09 +0900

    directive-modelをVite環境に合わせて更新。

--- chap05/directive-model/src/App.vue
+++ chap05/directive-model/src/App.vue
@@ -1,19 +1,12 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const inputNameModel = ref("双方向");
+</script>
+
 <template>
 	<section>
 		<input type="text" v-model="inputNameModel">
 		<p>{{inputNameModel}}</p>
 	</section>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const inputNameModel = ref("双方向");
-		return {
-			inputNameModel
-		};
-	}
-});
-</script>
