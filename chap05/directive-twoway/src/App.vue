43b565cc6862f59a95b068b9b8105d88cf652816
Shinzo SAITO <architshin@websarva.com>
2022-03-18 19:03:24 +0900

    directive-twowayをVite環境に合わせて更新。

--- chap05/directive-twoway/src/App.vue
+++ chap05/directive-twoway/src/App.vue
@@ -1,24 +1,16 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const inputName2Way = ref("双方向");
+const onInputName2Way = (event: Event): void => {
+	const element = event.target as HTMLInputElement;
+	inputName2Way.value = element.value;
+}
+</script>
+
 <template>
 	<section>
 		<input type="text" v-bind:value="inputName2Way" v-on:input="onInputName2Way">
 		<p>{{inputName2Way}}</p>
 	</section>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const inputName2Way = ref("双方向");
-		const onInputName2Way = function(event: Event): void {
-			const element = event.target as HTMLInputElement;
-			inputName2Way.value = element.value;
-		}
-		return {
-			inputName2Way,
-			onInputName2Way
-		};
-	}
-});
-</script>
