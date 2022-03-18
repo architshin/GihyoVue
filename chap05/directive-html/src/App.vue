9c2558456456c13d4740ce3ade02f0e9679eb824
Shinzo SAITO <architshin@websarva.com>
2022-03-18 19:57:13 +0900

    directive-htmlをVite環境に合わせて更新。

--- chap05/directive-html/src/App.vue
+++ chap05/directive-html/src/App.vue
@@ -1,17 +1,10 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const htmlStr = ref(`<a href="https://vuejs.org//">Vue.jsのTOPページ<a/>`);
+</script>
+
 <template>
 	<section>{{htmlStr}}</section>
 	<section v-html="htmlStr"></section>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const htmlStr = ref(`<a href="https://v3.vuejs.org/">Vue.jsのTOPページ<a/>`);
-		return {
-			htmlStr
-		};
-	}
-});
-</script>
