7ae5df26e99cbf098d9d388a7ba1eedc64b06789
Shinzo SAITO <architshin@websarva.com>
2022-03-18 20:09:42 +0900

    directive-cloakをVite環境に合わせて更新。

--- chap05/directive-cloak/src/App.vue
+++ chap05/directive-cloak/src/App.vue
@@ -1,19 +1,12 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const hello = ref("こんにちは!");
+</script>
+
 <template>
 	<p v-cloak>{{hello}}</p>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const hello = ref("こんにちは!");
-		return {
-			hello
-		};
-	}
-});
-</script>
 
 <style>
 [v-cloak] {
