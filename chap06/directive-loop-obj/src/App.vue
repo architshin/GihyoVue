5d36998ba268eb8c09f4d3c84550ad0621b5cf14
Shinzo SAITO <architshin@websarva.com>
2022-03-19 02:57:59 +0900

    directive-loop-objをVite環境に合わせて更新。

--- chap06/directive-loop-obj/src/App.vue
+++ chap06/directive-loop-obj/src/App.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import {ref} from "vue";
+const whiteLadyInit: {
+	id: number;
+	name: string;
+	price: number;
+	recipe: string;
+} = {
+	id: 2345,
+	name: "ホワイトレディ",
+	price: 1200,
+	recipe: "ジン30ml+コワントロー15ml+レモン果汁+15ml"
+};
+const whiteLady = ref(whiteLadyInit);
+</script>
+
 <template>
 	<dl>
 		<template
@@ -8,27 +24,3 @@
 		</template>
 	</dl>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const whiteLadyInit: {
-			id: number;
-			name: string;
-			price: number;
-			recipe: string;
-		} = {
-			id: 2345,
-			name: "ホワイトレディ",
-			price: 1200,
-			recipe: "ジン30ml+コワントロー15ml+レモン果汁+15ml"
-		};
-		const whiteLady = ref(whiteLadyInit);
-		return {
-			whiteLady
-		};
-	}
-});
-</script>
