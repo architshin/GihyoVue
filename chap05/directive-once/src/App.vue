58afdc85c11d0b5a08912154aa10497541632731
Shinzo SAITO <architshin@websarva.com>
2022-03-18 20:06:06 +0900

    directive-onceをVite環境に合わせて更新。

--- chap05/directive-once/src/App.vue
+++ chap05/directive-once/src/App.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const price = ref(1000);
+</script>
+
 <template>
 	<section>
 		<input type="number" v-model="price">円<br>
@@ -5,16 +11,3 @@
 		<p v-once>金額は{{price}}円です。</p>
 	</section>
 </template>
-
-<script lang="ts">
-import {defineComponent, ref} from "vue";
-
-export default defineComponent({
-	setup() {
-		const price = ref(1000);
-		return {
-			price
-		};
-	}
-});
-</script>
