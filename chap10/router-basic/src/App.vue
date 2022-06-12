35efaf8ed3a7fc4903d951674b923dad512cb591
Shinzo SAITO <architshin@websarva.com>
2022-06-13 00:03:29 +0900

    router-basicを初校に合わせて修正。

--- chap10/router-basic/src/App.vue
+++ chap10/router-basic/src/App.vue
@@ -4,7 +4,7 @@ import {RouterView} from "vue-router";
 import type {Member} from "@/interfaces";
 
 const memberList = new Map<number, Member>();
-memberList.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "ちょ〜イケメン。"});
+memberList.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり。"});
 memberList.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});
 provide("memberList", reactive(memberList));
 </script>
