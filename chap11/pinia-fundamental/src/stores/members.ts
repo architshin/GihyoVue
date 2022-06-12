615c27eecb4bbd96a2b55ba5b5d0599ba055153d
Shinzo SAITO <architshin@websarva.com>
2022-06-13 00:25:28 +0900

    pinia-fundamentalを初校に合わせて修正。

--- chap11/pinia-fundamental/src/stores/members.ts
+++ chap11/pinia-fundamental/src/stores/members.ts
@@ -10,19 +10,19 @@ export const useMembersStore = defineStore({
 	state: (): State => {
 		return {
 			memberList: new Map<number, Member>()
-		}
+		};
 	},
 	getters: {
 		getById: (state) => {
 			return (id: number): Member => {
 				const member = state.memberList.get(id) as Member;
 				return member;
-			}
+			};
 		}
 	},
 	actions: {
 		initList(): void {
-			this.memberList.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "ちょ〜イケメン。"});
+			this.memberList.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり。"});
 			this.memberList.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});	
 		},
 		addMember(member: Member): void {
